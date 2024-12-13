6bca17a34c669639e15b58ca2d35a09a3a839c4f
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2024-12-13 12:04:40 +0100

    fix index.css + add js function Fetch in MainSection.jsx

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Sidebar from "./Sidebar";
 
 const MainSection = () => {
+  // STATE
+  const [rockSongs, setRockSongs] = useState([]);
+  const [popSongs, setPopSongs] = useState([]);
+  const [hipHopSongs, setHipHopSongs] = useState([]);
+
+  // chimata Fetch GET
+  const fillMusicSection = async (artistName, setSongsFunction) => {
+    try {
+      let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artistName}`);
+      if (response.ok) {
+        let { data } = await response.json();
+        setSongsFunction(data.slice(0, 4)); //Per adesso prendo solo i primi 4 album
+      } else {
+        throw new Error("Error in fetching songs");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
+  // API degli artisti componentDidMount()
+  useEffect(() => {
+    fillMusicSection("queen", setRockSongs);
+    fillMusicSection("katyperry", setPopSongs);
+    fillMusicSection("eminem", setHipHopSongs);
+  }, []);
+
+  // Funzione per rendere il contenuto dinamico di ogni canzone
+  const albumCard = (singleSong) => {
+    return (
+      <div className="col text-center" key={singleSong.id}>
+        <img className="img-fluid" src={singleSong.album.cover_medium} alt="track" />
+        <p>
+          Track: {singleSong.title}
+          <br />
+          Artist: {singleSong.artist.name}
+        </p>
+      </div>
+    );
+  };
+
   return (
     <Container fluid>
       <Row>
@@ -20,7 +62,9 @@ const MainSection = () => {
             <Col xs={10}>
               <div id="rock">
                 <h2>Rock Classics</h2>
-                <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3" id="rockSection" />
+                <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3" id="rockSection">
+                  {rockSongs.map(albumCard)}
+                </Row>
               </div>
             </Col>
           </Row>
@@ -28,7 +72,9 @@ const MainSection = () => {
             <Col xs={10}>
               <div id="pop">
                 <h2>Pop Culture</h2>
-                <Row xs={1} sm={2} lg={3} xl={4} className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3" id="popSection" />
+                <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3" id="popSection">
+                  {popSongs.map(albumCard)}
+                </Row>
               </div>
             </Col>
           </Row>
@@ -36,7 +82,9 @@ const MainSection = () => {
             <Col xs={10}>
               <div id="hiphop">
                 <h2>#HipHop</h2>
-                <Row xs={1} sm={2} lg={3} xl={4} className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3" id="hipHopSection" />
+                <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3" id="hipHopSection">
+                  {hipHopSongs.map(albumCard)}
+                </Row>
               </div>
             </Col>
           </Row>
